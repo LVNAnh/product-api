f64cd375a76020f40fc4fe3ca7ea1d1d36c4708b
LVNAnh <levunhatanh1997@gmail.com>
2024-12-17 14:50:06 +0700

    hehe

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,12 @@
-# Base image
 FROM node:16
 
-# Set working directory
 WORKDIR /usr/src/app
 
-# Copy project files
 COPY package*.json ./
 RUN npm install
 
-# Copy source code
 COPY . .
 
-# Expose port 3000
 EXPOSE 3000
 
-# Command to run the server
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const products = [
   { id: 1, title: "Product A", stock: 100, price: 50 },
   { id: 2, title: "Product B", stock: 200, price: 30 },
   { id: 3, title: "Product C", stock: 50, price: 70 },
+  { id: 4, title: "Product D", stock: 100, price: 80 },
 ];
 
 app.get("/product", (req, res) => {
